42bd12a55a1471afd48626d6431f54fdfe609347
Yosuke Fujii <yosuke.fujii@O-04701-MAC.local>
2014-08-12 20:44:58 +0900

    第二回done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	// 移動速度
+	public float speed = 5;
+
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		// 右左
+		float x = Input.GetAxisRaw ("Horizontal");
+		// 上下
+		float y = Input.GetAxisRaw ("Vertical");
+
+		// 移動方向決定
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		// 移動する向きとスピード
+		rigidbody2D.velocity = direction * speed;
 	}
 }
