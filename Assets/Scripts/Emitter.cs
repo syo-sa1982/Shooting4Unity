f85a29b7cc1ec4a2dc1abd1d248ba9518cc27524
Yosuke Fujii <yosuke.fujii@O-04701-MAC.local>
2014-09-02 10:01:24 +0900

    ファイルaddするの忘れてた

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour 
+{
+	// Waveプレハブを格納する
+	public GameObject[] waves;
+
+	// 現在のWave
+	private int currentWave;
+
+	// Use this for initialization
+	IEnumerator Start () 
+	{
+		// Waveが存在しなければコルーチンを終了する
+		if (waves.Length == 0) { yield break; }
+
+		while (true) {
+			// Wave作成
+			GameObject wave =(GameObject)Instantiate(waves[currentWave], transform.position, Quaternion.identity);
+		}
+
+	}
+}
