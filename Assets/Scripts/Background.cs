b4409ede4a05d200e3633b5e2387736de2a18b00
Yosuke Fujii <yosuke.fujii@O-04701-MAC.local>
2014-09-02 09:35:37 +0900

    第6回done

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour 
+{
+	// スクロールスピード
+	public float speed = 0.1f;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// Y軸を時間経過で0から１に変化させる
+		float y = Mathf.Repeat (Time.time * speed, 1);
+
+		// Y軸をずらすオフセット作成
+		Vector2 offset = new Vector2 (0, y);
+
+		// マテリアルにオフセットを設定
+		renderer.sharedMaterial.SetTextureOffset ("_MainTex", offset);
+	}
+}
