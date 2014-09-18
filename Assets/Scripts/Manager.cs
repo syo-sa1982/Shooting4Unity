194471f4b5fddbd7702e3e45b5b7b45770193238
Yosuke Fujii <yosuke.fujii@O-04701-MAC.local>
2014-09-18 09:15:49 +0900

    Managerが追加されてなかった

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour 
+{
+	public GameObject player;
+
+	private GameObject title;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Titleゲームオブジェクトを検索し取得する
+		title = GameObject.Find ("Title");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// ゲーム中ではなく、Xキーが押されたらtrueを返す。
+		if (IsPlaying () == false && Input.GetKeyDown (KeyCode.X)) {
+			GameStart ();
+		}
+	}
+
+	void GameStart () 
+	{
+		// ゲームスタート時に、タイトルを非表示にしてプレイヤーを作成する
+		title.SetActive (false);
+		Instantiate (player, player.transform.position, player.transform.rotation);
+	}
+
+	public void GameOver () 
+	{
+		// ゲームオーバー時に、タイトルを表示する
+		title.SetActive (true);
+	}
+
+
+	public bool IsPlaying()
+	{
+		// ゲーム中かどうかはタイトルの表示/非表示で判断する
+		return title.activeSelf == false;
+	}
+}
